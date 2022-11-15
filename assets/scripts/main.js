c01f276d1282d4e744cbc8bbd7855c00b07b409e
Austin Marking <amarking@ucsd.edu>
2022-11-14 23:48:22 -0800

    changes

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -33,7 +33,7 @@ async function init() {
  * of installing it and getting it running
  */
 function initializeServiceWorker() {
-  // EXPLORE - START (All explore numbers start with B)
+   // EXPLORE - START (All explore numbers start with B)
   /*******************/
   // ServiceWorkers have many uses, the most common of which is to manage
   // local caches, intercept network requests, and conditionally serve from
@@ -45,15 +45,23 @@ function initializeServiceWorker() {
   // We first must register our ServiceWorker here before any of the code in
   // sw.js is executed.
   // B1. TODO - Check if 'serviceWorker' is supported in the current browser
-  // B2. TODO - Listen for the 'load' event on the window object.
-  // Steps B3-B6 will be *inside* the event listener's function created in B2
-  // B3. TODO - Register './sw.js' as a service worker (The MDN article
-  //            "Using Service Workers" will help you here)
-  // B4. TODO - Once the service worker has been successfully registered, console
-  //            log that it was successful.
-  // B5. TODO - In the event that the service worker registration fails, console
-  //            log that it has failed.
-  // STEPS B6 ONWARDS WILL BE IN /sw.js
+  if ('serviceWorker' in navigator) {  
+    // B2. TODO - Listen for the 'load' event on the window object.
+    window.addEventListener('load', function(){
+      // Steps B3-B6 will be *inside* the event listener's function created in B2
+      // B3. TODO - Register './sw.js' as a service worker (The MDN article
+      //            "Using Service Workers" will help you here)
+      navigator.serviceWorker.register('./sw.js', {scope: './'}).then((registration) => {
+        // B4. TODO - Once the service worker has been successfully registered, console
+        //            log that it was successful.
+        console.log('Service worker registration succeeded:', registration);
+      }, /*catch*/ (error) => {
+        // B5. TODO - In the event that the service worker registration fails, console
+        //            log that it has failed.    
+        console.error(`Service worker registration failed: ${error}`);
+      });
+    })
+  }
 }
 
 /**
